9355f56101f9c5897e54675a7042da139a6d195a
Maddev25 <devanb4u@gmail.com>
2017-08-15 23:41:37 +1000

    15Aug17

--- run_analysis.R
+++ run_analysis.R
@@ -1,4 +1,18 @@
 ## Getting and Cleaning Data Week 4 Assignment 
 ## Author Vasudevan D 
 ## Date   13/Aug/2017 
-## Goal is to create a tiny Data set and store proper instructions to generate that
+## Goal is to create a tiny Data set and store proper instructions for the given Raw Data 
+
+##1. Download & unzip the Raw data set file in data folder under working directory
+
+##if(!file.exists("./data")){dir.create("./data")}
+##fileURL <- "https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip"
+##download.file(fileURL, destfile = "./data/UCIDataSet.zip")
+##unzip(zipfile = "./data/UCIDataSet.zip", exdir = "./data" )
+
+##2. List the files available in the unzipped folder 
+fp <- file.path("./data", "UCI HAR Dataset")
+fileSet <- list.files(path = fp , recursive = TRUE) 
+fileSet
+
+

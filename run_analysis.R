1c4617b69fd096cb18056bd46f5e3a6ac055985f
Maddev25 <devanb4u@gmail.com>
2017-08-15 22:58:01 +1000

    First

--- run_analysis.R
+++ run_analysis.R
@@ -1,4 +1,4 @@
 ## Getting and Cleaning Data Week 4 Assignment 
 ## Author Vasudevan D 
-## Date   15/Aug/2017 
+## Date   13/Aug/2017 
 ## Goal is to create a tiny Data set and store proper instructions to generate that
